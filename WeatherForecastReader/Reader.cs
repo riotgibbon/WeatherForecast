7b04c9b58ade76d775e45dc6165775f85b825f6d
Toby Evans <tobysevans@gmail.com>
2012-11-15 01:53:24 +0000

    Removed breaking extra references

--- WeatherForecastReader/Reader.cs
+++ WeatherForecastReader/Reader.cs
@@ -1,13 +1,7 @@
-﻿using System;
-
-using System.Collections.Generic;
+﻿
 using System.Linq;
 using System.Net;
-using System.Net.Http;
-using System.Web.Http;
-using System.Text;
-using System.Threading.Tasks;
-using System.Runtime.Serialization.Json;
+
 using Newtonsoft.Json.Linq;
 
 
