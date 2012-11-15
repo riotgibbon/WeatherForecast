e63a826f43088830ad143828588580cf59957779
Toby Evans <tobysevans@gmail.com>
2012-11-15 01:49:37 +0000

    Wired reader into WebAPI

---
+++ WeatherForecastMVC/Controllers/ForecastController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using WeatherForecastReader;
+
+namespace WeatherForecastMVC.Controllers
+{
+    public class ForecastController : ApiController
+    {
+       
+        
+        
+        // GET api/values/5
+        public string Get(int id)
+        {
+            var weatherSource = new LiveWeatherUndergroundSource();
+            var weatherReader = new WeatherUndergroundReader(weatherSource);
+            var temp = weatherReader.GetPeriodTemp(id);
+            return temp;
+        }
+    }
+}
