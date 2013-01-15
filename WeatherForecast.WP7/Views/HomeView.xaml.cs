8ec0c95e4beeac13650457cabf876d315b6deffa
Toby Evans (YEPS82045) <toby.evans@hibu.com>
2013-01-15 20:42:03 +0000

    Added WP7 app

---
+++ WeatherForecast.WP7/Views/HomeView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Cirrious.MvvmCross.WindowsPhone.Views;
+using WeatherForecast.MVVMCore.ViewModels;
+
+namespace WeatherForecast.WP7.Views
+{
+    public partial class HomeView 
+        : BaseHomeView
+    {
+        public HomeView()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class BaseHomeView : MvxPhonePage<HomeViewModel>
+    {
+    }
+}
