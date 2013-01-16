8953393604c7821f10e13e378ac40b4090399a1b
Toby Evans (YEPS82045) <toby.evans@hibu.com>
2013-01-16 15:42:35 +0000

    Removed WP7 app

--- WeatherForecast.WP7/NativeConverters/NativeVisibilityConverter.cs
+++
@@ -1,9 +0,0 @@
-using Cirrious.MvvmCross.Plugins.Visibility;
-using Cirrious.MvvmCross.WindowsPhone.Platform.Converters;
-
-namespace TwitterSearch.UI.WP7.NativeConverters
-{
-    public class NativeVisibilityConverter : MvxNativeValueConverter<MvxVisibilityConverter>
-    {
-    }
-}
